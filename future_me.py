36d251fba0f80bfa3815ae453b0032bd1fa2c2d8
Anjali Krishna <anjali.krishna94960@gmail.com>
2026-06-26 23:21:56 +0530

    Day 1 - First Python Program

---
+++ future_me.py
@@ -0,0 +1,14 @@
+name=str(input("Please enter your name : "))
+age=int(input("Please enter yur age : "))
+dream=str(input("What is your dream : "))
+print()
+
+print(f"Hello {name}!")
+print()
+print(f" You're {age} years old.")
+print(f"Your dream is to {dream}.")
+
+print("remember:")
+print("Every expert was once a Day 1.")
+print()
+print("Now go build.")
